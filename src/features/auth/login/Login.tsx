bccda8b917226bd766867a399ebc6573f502230a
fefelov-danil <fefelov.danil@gmail.com>
2022-09-17 23:15:14 +0300

    Update: Layout Login (txs, css)

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 
+import { NavLink } from 'react-router-dom'
+
+import s from './Login.module.css'
+
+import { Button } from 'common/button/Button'
+import { Checkbox } from 'common/checkbox/Checkbox'
+import { InputText } from 'common/input/InputText'
+
 export const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={s.loginPage}>
+      <div className={s.loginContainer}>
+        <h1>Sign in</h1>
+        <p className={s.fieldName}>Email:</p>
+        <InputText />
+        <p className={s.fieldName}>Password:</p>
+        <InputText />
+        <Checkbox>Remember me</Checkbox>
+        <p className={s.forgotPassword}>
+          <NavLink to={'password-recovery'}>Forgot Password?</NavLink>
+        </p>
+        <Button className={s.signInBtn}>Sign In</Button>
+      </div>
     </div>
   )
 }
