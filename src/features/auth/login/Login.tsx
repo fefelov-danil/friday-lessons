b3f0e0a8dfa61be089a7d542fad0b48b08ee6542
fefelov-danil <fefelov.danil@gmail.com>
2022-09-17 23:58:27 +0300

    Update: Formik for Login & change Login.module.css

--- src/features/auth/login/Login.tsx
+++ src/features/auth/login/Login.tsx
@@ -1,27 +1,80 @@
 import React from 'react'
 
+import { useFormik } from 'formik'
 import { NavLink } from 'react-router-dom'
 
 import s from './Login.module.css'
 
 import { Button } from 'common/button/Button'
 import { Checkbox } from 'common/checkbox/Checkbox'
-import { InputText } from 'common/input/InputText'
+import { InputPassword } from 'common/inputPassword/InputPassword'
+import { InputText } from 'common/inputText/InputText'
+
+type FormikErrorType = {
+  email?: string
+  password?: string
+  rememberMe?: boolean
+}
 
 export const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+    validate: values => {
+      const errors: FormikErrorType = {}
+
+      if (!values.email) {
+        errors.email = 'required'
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+      }
+
+      if (!values.password) {
+        errors.password = 'required'
+      } else if (values.password.length < 3) {
+        errors.password = 'Пароль должен быть длиннее 2х символов'
+      }
+
+      return errors
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2))
+      // dispatch(loginTC(values))
+    },
+  })
+
   return (
     <div className={s.loginPage}>
       <div className={s.loginContainer}>
         <h1>Sign in</h1>
-        <p className={s.fieldName}>Email:</p>
-        <InputText />
-        <p className={s.fieldName}>Password:</p>
-        <InputText />
-        <Checkbox>Remember me</Checkbox>
-        <p className={s.forgotPassword}>
-          <NavLink to={'password-recovery'}>Forgot Password?</NavLink>
-        </p>
-        <Button className={s.signInBtn}>Sign In</Button>
+        <form onSubmit={formik.handleSubmit}>
+          <p className={s.fieldName}>Email:</p>
+          <label className={s.labelField}>
+            <InputText {...formik.getFieldProps('email')} />
+            {formik.touched.email && formik.errors.email && (
+              <p className={s.fieldError}>{formik.errors.email}</p>
+            )}
+          </label>
+          <p className={s.fieldName}>Password:</p>
+          <label className={s.labelField}>
+            <InputPassword {...formik.getFieldProps('password')} />
+            {formik.touched.password && formik.errors.password && (
+              <p style={{ color: 'red' }}>{formik.errors.password}</p>
+            )}
+          </label>
+          <label className={s.labelCheckbox}>
+            <Checkbox {...formik.getFieldProps('rememberMe')}>Remember me</Checkbox>
+          </label>
+          <p className={s.forgotPassword}>
+            <NavLink to={'password-recovery'}>Forgot Password?</NavLink>
+          </p>
+          <Button type={'submit'} className={s.signInBtn}>
+            Sign In
+          </Button>
+        </form>
       </div>
     </div>
   )
