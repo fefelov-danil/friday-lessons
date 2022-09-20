3ac9d9ea0eed749b38fbca9d8c02596d16b3c893
fefelov-danil <fefelov.danil@gmail.com>
2022-09-20 16:25:45 +0300

    Update: store.ts - add appReducer in rootReducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
-import thunk, { ThunkDispatch } from 'redux-thunk'
+import thunk, { ThunkAction, ThunkDispatch } from 'redux-thunk'
 
+import { AppActionsType, appReducer } from 'app/app-reducer'
 import { AuthActionsType, authReducer } from 'features/auth/auth-reducer'
 
 const rootReducer = combineReducers({
   auth: authReducer,
+  app: appReducer,
 })
 
 export type RootReducerType = ReturnType<typeof rootReducer>
@@ -12,8 +14,14 @@ export const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = ThunkDispatch<RootState, unknown, AllActionsType>
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  AllActionsType
+>
 
-export type AllActionsType = AuthActionsType
+export type AllActionsType = AuthActionsType | AppActionsType
 
 // @ts-ignore
 window.store = store
