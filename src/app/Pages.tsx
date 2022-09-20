1915cef1e1567502b566175e3498b6a2b2c2dc5c
fefelov-danil <fefelov.danil@gmail.com>
2022-09-20 16:28:59 +0300

    Update: Pages.tsx - Added content display condition. If logged in, then the profile is displayed, if not, then the login.

--- src/app/Pages.tsx
+++ src/app/Pages.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 
 import { Navigate, Route, Routes } from 'react-router-dom'
 
+import { useAppSelector } from 'app/hooks'
 import { PageNotFound } from 'common/404/PageNotFound'
-import { ChangePassword } from 'features/auth/change-password/ChangePassword'
 import { Login } from 'features/auth/login/Login'
-import { PasswordRecovery } from 'features/auth/password-recovery/PasswordRecovery'
 import { Profile } from 'features/auth/profile/Profile'
-import { Registration } from 'features/auth/registration/Registration'
 
 export const PATH = {
   LOGIN: '/login',
@@ -18,15 +16,24 @@ export const PATH = {
 }
 
 export const Pages = () => {
+  const isVerifyLogin = useAppSelector(state => state.auth.isVerifyLogin)
+
   return (
     <div>
       <Routes>
-        <Route path={'/'} element={<Navigate to={PATH.LOGIN} />} />
-        <Route path={PATH.LOGIN} element={<Login />} />
-        <Route path={PATH.REGISTRATION} element={<Registration />} />
-        <Route path={PATH.PASSWORD_RECOVERY} element={<PasswordRecovery />} />
-        <Route path={PATH.CHANGE_PASSWORD} element={<ChangePassword />} />
-        <Route path={PATH.PROFILE} element={<Profile />} />
+        {isVerifyLogin ? (
+          <>
+            <Route path={'/'} element={<Navigate to={PATH.PROFILE} />} />
+            <Route path={PATH.LOGIN} element={<Navigate to={PATH.PROFILE} />} />
+            <Route path={PATH.PROFILE} element={<Profile />} />
+          </>
+        ) : (
+          <>
+            <Route path={'/'} element={<Navigate to={PATH.LOGIN} />} />
+            <Route path={PATH.PROFILE} element={<Navigate to={PATH.LOGIN} />} />
+            <Route path={PATH.LOGIN} element={<Login />} />
+          </>
+        )}
         <Route path={'/404'} element={<PageNotFound />} />
         <Route path={'*'} element={<Navigate to={'/404'} />} />
       </Routes>
