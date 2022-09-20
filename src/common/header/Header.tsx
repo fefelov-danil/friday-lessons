633af956dc0fddb2c59f1e134b6b82dc00bd0631
fefelov-danil <fefelov.danil@gmail.com>
2022-09-20 16:39:22 +0300

    Update: Header.tsx & css - styles and different display depending on login

--- src/common/header/Header.tsx
+++ src/common/header/Header.tsx
@@ -2,16 +2,20 @@ import React from 'react'
 
 import { NavLink } from 'react-router-dom'
 
+import { useAppSelector } from 'app/hooks'
+import logo from 'assets/images/logo.png'
 import s from 'common/header/Header.module.css'
 
 export const Header = () => {
+  const isVerifyLogin = useAppSelector(state => state.auth.isVerifyLogin)
+
   return (
     <div className={s.header}>
-      <NavLink to={'login'}>Login</NavLink>
-      <NavLink to={'registration'}>Registration</NavLink>
-      <NavLink to={'password-recovery'}>Password recovery</NavLink>
-      <NavLink to={'change-password'}>Change password</NavLink>
-      <NavLink to={'profile'}>Profile</NavLink>
+      <div className={'container'}>
+        <img src={logo} alt={''} />
+        {isVerifyLogin && <NavLink to={'profile'}>Profile</NavLink>}
+        {!isVerifyLogin && <NavLink to={'login'}>Sign in</NavLink>}
+      </div>
     </div>
   )
 }
