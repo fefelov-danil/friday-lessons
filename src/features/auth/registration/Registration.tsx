fd170e1bd4ff743ef72d21332b6f3d4bdc7a8eee
VadymHromyk <hromuk.vadim@gmail.com>
2022-09-20 20:18:25 +0300

    temporary files

--- src/features/auth/registration/Registration.tsx
+++ src/features/auth/registration/Registration.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { RootState } from 'app/store'
+import { Button } from 'common/button/Button'
+import { InputText } from 'common/input/InputText'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { Navigate, NavLink } from 'react-router-dom'
+import { signUpTC } from './registration-reducer'
 
 export const Registration = () => {
+
+  const dispatch = useDispatch<any>()
+
+  const errMessage = useSelector<RootState, string>(state => state.registration.errorMessage)
+
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [repeatPassword, setRepeatPassword] = useState<string>('');
+  const [mess, setMess] = useState<string>('');
+
+  const confirmRegistrationHandler = () => {
+    if (password === repeatPassword) {
+      dispatch(signUpTC(email, password))
+      setEmail('')
+      setPassword('')
+      setRepeatPassword('')
+    } else {
+      setMess('confirm your password currectly')
+    }
+  }
+
+  // if ('logged in') return <Navigate to={'/main page'}/>
+
   return (
     <div>
       <h1>Registration</h1>
+
+      <InputText
+        placeholder='email'
+        value={email}
+        onChange={(e) => { setEmail(e.currentTarget.value) }}
+      />
+      <InputText
+        placeholder='password'
+        value={password}
+        onChange={(e) => { setPassword(e.currentTarget.value) }}
+      />
+      <InputText
+        placeholder='repeatPassword'
+        value={repeatPassword}
+        onChange={(e) => { setRepeatPassword(e.currentTarget.value) }}
+      />
+      <Button onClick={confirmRegistrationHandler}>OK</Button>
+      {errMessage && <div>{errMessage}</div>}
+      {mess && <div>{mess}</div>}
+
+      <p>Already have an account?</p>
+      <NavLink to={'/login'}>Sign In</NavLink>
+
     </div>
   )
 }
--- src/features/auth/registration/registration-reducer.ts
+++ src/features/auth/registration/registration-reducer.ts
@@ -1,21 +1,44 @@
-const initialState = {}
+import { registrationAPI } from './registration-api';
+
+const initialState: RegistrationStateType = {
+  isRegistered: false,
+  errorMessage: '',
+}
 
 export const registrationReducer = (
   state: RegistrationStateType = initialState,
   action: RegistrationActionsType
 ): RegistrationStateType => {
   switch (action.type) {
+    case "SET-IS-REGISTERED":
+      return { ...state, isRegistered: action.isRegistered }
+    case "ERROR":
+      return { ...state, errorMessage: action.errorMessage }
     default:
       return state
   }
 }
 
 // Actions
-export const exampleRegistrationAC = () => ({ type: 'EXAMPLE' } as const)
+export const setSuccesRegistration = (isRegistered: boolean) => ({ type: "SET-IS-REGISTERED", isRegistered } as const)
+export const setErrorMessage = (errorMessage: string) => ({ type: "ERROR", errorMessage } as const)
 
 // Thunks
+export const signUpTC = (email: string, password: string) => (dispatch: any) => {
+  registrationAPI.signUp(email, password)
+    .then(res => {
+      dispatch(setSuccesRegistration(true))
+    })
+    .catch((err) => {
+      dispatch(setErrorMessage(err.response.data.error))
+     })
+    .finally(() => { })
+}
 
 // Types
-type RegistrationStateType = {}
+type RegistrationStateType = {
+  isRegistered: boolean
+  errorMessage: string
+}
 
-type RegistrationActionsType = ReturnType<typeof exampleRegistrationAC>
+type RegistrationActionsType = ReturnType<typeof setSuccesRegistration> | ReturnType<typeof setErrorMessage>
