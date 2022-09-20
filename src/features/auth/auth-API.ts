b3843120fa326603b61e50d5c2055d6c6d63325b
fefelov-danil <fefelov.danil@gmail.com>
2022-09-20 16:42:07 +0300

    New: auth-API.ts - authMe and login

--- src/features/auth/auth-API.ts
+++ src/features/auth/auth-API.ts
@@ -1,8 +1,37 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'http://localhost:7542',
+  baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
   withCredentials: true,
 })
 
-export const authAPI = {}
+export const authAPI = {
+  authMe() {
+    return instance.post<ResponseType>('/auth/me', {})
+  },
+  login(values: loginValues) {
+    return instance.post<ResponseType>('/auth/login', values)
+  },
+}
+
+// types
+export type loginValues = {
+  email: string
+  password: string
+  rememberMe?: boolean
+}
+type ResponseType = {
+  _id: string
+  email: string
+  name: string
+  avatar?: string
+  publicCardPacksCount: number // количество колод
+
+  created: Date
+  updated: Date
+  isAdmin: boolean
+  verified: boolean // подтвердил ли почту
+  rememberMe: boolean
+
+  error?: string
+}
