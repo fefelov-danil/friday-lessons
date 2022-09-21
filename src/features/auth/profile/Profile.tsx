67382b33743a1da00bc9bb4b89127065184ecff1
fefelov-danil <fefelov.danil@gmail.com>
2022-09-21 13:12:31 +0300

    Update: Profile.tsx - for the time being

--- src/features/auth/profile/Profile.tsx
+++ src/features/auth/profile/Profile.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+import { useAppDispatch } from 'app/hooks'
+import { deleteMe } from 'features/auth/auth-reducer'
+
 export const Profile = () => {
+  const dispatch = useAppDispatch()
+
   return (
     <div>
       <h1>Profile</h1>
+      <button onClick={() => dispatch(deleteMe())}>Log Out</button>
     </div>
   )
 }
