99640351ce1d7dbbd32e4ac110e27c0667d6fffd
fefelov-danil <fefelov.danil@gmail.com>
2022-09-21 13:06:13 +0300

    Update: route Change Password

--- src/app/Pages.tsx
+++ src/app/Pages.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { useAppSelector } from 'app/hooks'
 import { PageNotFound } from 'common/404/PageNotFound'
+import { ChangePassword } from 'features/auth/change-password/ChangePassword'
 import { Login } from 'features/auth/login/Login'
 import { PasswordRecovery } from 'features/auth/password-recovery/PasswordRecovery'
 import { Profile } from 'features/auth/profile/Profile'
@@ -13,7 +14,7 @@ export const PATH = {
   LOGIN: '/login',
   REGISTRATION: '/registration',
   PASSWORD_RECOVERY: '/password-recovery',
-  CHANGE_PASSWORD: '/change-password',
+  CHANGE_PASSWORD: '/set-new-password/:token',
   PROFILE: '/profile',
 }
 
@@ -38,6 +39,7 @@ export const Pages = () => {
         )}
         <Route path={PATH.REGISTRATION} element={<Registration />} />
         <Route path={PATH.PASSWORD_RECOVERY} element={<PasswordRecovery />} />
+        <Route path={PATH.CHANGE_PASSWORD} element={<ChangePassword />} />
         <Route path={'/404'} element={<PageNotFound />} />
         <Route path={'*'} element={<Navigate to={'/404'} />} />
       </Routes>
