e4f08b9945c87f02214353161cc8ac7536ee61d3
fefelov-danil <fefelov.danil@gmail.com>
2022-09-21 13:09:43 +0300

    Update: auth-API.ts

--- src/features/auth/auth-API.ts
+++ src/features/auth/auth-API.ts
@@ -1,26 +1,48 @@
-import axios, { AxiosResponse } from 'axios'
+import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+  baseURL: 'https://neko-back.herokuapp.com/2.0',
   withCredentials: true,
 })
 
 export const authAPI = {
   authMe() {
-    return instance.post<ResponseType>('/auth/me', {})
+    return instance.post<ResponseUserType>('/auth/me', {})
   },
-  login(values: loginValues) {
-    return instance.post<ResponseType>('/auth/login', values)
+  login(values: loginValuesType) {
+    return instance.post<ResponseUserType>('/auth/login', values)
+  },
+  forgotPassword(values: forgotPasswordValuesType) {
+    return instance.post<ResponseType>('/auth/forgot', values)
+  },
+  changePassword(values: changePasswordType) {
+    return instance.post<ResponseType>('/auth/set-new-password', values)
+  },
+  deleteMe() {
+    return instance.delete<ResponseType>('/auth/me', {})
   },
 }
 
 // types
-export type loginValues = {
+export type loginValuesType = {
   email: string
   password: string
   rememberMe?: boolean
 }
+export type forgotPasswordValuesType = {
+  email: string
+  from: string
+  message: string
+}
+export type changePasswordType = {
+  password: string
+  resetPasswordToken: string
+}
 type ResponseType = {
+  info: string
+  error: string
+}
+type ResponseUserType = {
   _id: string
   email: string
   name: string
