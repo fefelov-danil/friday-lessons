8a16cf5e1f883ebda961b734c8b781eccfa320ba
Roman Petrakovskiy <roman.petrakovskiy@gmail.com>
2022-09-26 20:42:15 +0700

    Update: redirect if not logged in

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -3,6 +3,7 @@ import { Dispatch } from 'redux'
 
 import { appAlertAC, appSetStatusAC } from '../app/app-reducer'
 import { AllActionsType } from '../app/store'
+import { authIsLoggedInAC } from '../features/auth/auth-reducer'
 
 export const handleServerError = (
   e: Error | AxiosError<{ error: string }>,
@@ -14,6 +15,10 @@ export const handleServerError = (
     const error = err.response?.data
       ? (err.response.data as { error: string }).error
       : 'Some error, try again'
+
+    if (err.response?.status === 401) {
+      dispatch(authIsLoggedInAC(false))
+    }
 
     dispatch(appAlertAC(error, 'error'))
   } else {
