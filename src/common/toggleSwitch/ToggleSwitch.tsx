d448024d122c0008e4c28441d9440475884a4c6b
Roman Petrakovskiy <roman.petrakovskiy@gmail.com>
2022-09-27 16:12:37 +0700

    New: toggle switch

---
+++ src/common/toggleSwitch/ToggleSwitch.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import s from './ToggleSwitch.module.css'
+
+type PropsType = {
+  selected: boolean
+  param1: string
+  param2: string
+  onChange: (value: boolean) => void
+  className?: string
+  disabled?: boolean
+}
+
+export const ToggleSwitch = (props: PropsType) => {
+  return (
+    <div className={s.toggleSwitch}>
+      <div
+        className={props.selected ? s.param : s.param + ' ' + s.active}
+        onClick={() => !props.disabled && props.onChange(false)}
+      >
+        {props.param1}
+      </div>
+      <div
+        className={props.selected ? s.param + ' ' + s.active : s.param}
+        onClick={() => !props.disabled && props.onChange(true)}
+      >
+        {props.param2}
+      </div>
+    </div>
+  )
+}
