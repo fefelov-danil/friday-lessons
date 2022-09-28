feeec01e8ecbeed415b32cbdf01c8ae82fc257b7
VadymHromyk <hromuk.vadim@gmail.com>
2022-09-28 13:50:39 +0300

    New: added Learning component

---
+++ src/features/learning/Learning.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const grades = [
+  { value: 1, label: 'Did not know' },
+  { value: 2, label: 'Forgot' },
+  { value: 3, label: 'A lot of thought' },
+  { value: 4, label: 'Confused' },
+  { value: 5, label: 'Knew the answer' },
+]
+
+export const Learning = () => {
+  return <div></div>
+}
